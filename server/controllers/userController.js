28f27fc1ade3980ad5244e99c6b49de3922e53ce
nassir <nassirmz@gmail.com>
2016-12-12 00:38:24 -0800

    Hash password using bcrypt package

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,14 @@
 const db = require('../db');
+const helpers = require('../utils/helpers.js');
 
 module.exports = {
   createUser(req, res, next) {
     let queryStr = 'Insert into users (name, email, password) values ($1, $2, $3) returning id;';
     console.log('what is req body', req.body);
     let { name, email, password } = req.body;
-    console.log('from database', name, email, password);
-    db.query(queryStr, [name, email, password])
+    let hashed_pwd = helpers.hashPassword(password);
+    console.log('from database', name, email, hashed_pwd);
+    db.query(queryStr, [name, email, hashed_pwd])
       .then(function (data) {
         res.json(data);
       })
--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,1 +1,18 @@
+const bcrypt = require('bcrypt');
 
+//Create a password salt
+const salt = bcrypt.genSaltSync(10);
+
+module.exports = {
+  hashPassword(passwordFromUser) {
+    return bcrypt.hashSync(passwordFromUser, salt);
+  },
+  checkForPassword(passwordFromUser, rows) {
+    //if password matches
+    if (bcrypt.hashSync(passwordFromUser, salt) === rows[0].password) {
+      return true;
+    }
+    
+    return false;
+  }
+}
