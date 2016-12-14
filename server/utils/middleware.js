aa8de0d5045290a944c480eceb17434f4c204341
nassir <nassirmz@gmail.com>
2016-12-13 20:22:49 -0800

    Use expressJWT middleware

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,9 +1,12 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const expressJWT = require('express-jwt');
+const path = require('path');
 
 module.exports = (app, express) => {
   app.use(morgan('dev'));
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(express.static(__dirname + '/../../public'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(expressJWT({ secret: 'mynameistom' }).unless({ path: ['/users/login', '/users', '/polls', '/'] }));
+  app.use(express.static(path.join(__dirname, '/../../public.js')));
 };
