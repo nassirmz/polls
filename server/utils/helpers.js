4e1d9f3c3903a342d2e4b790a5c9248381374a28
nassir <nassirmz@gmail.com>
2016-12-13 20:23:37 -0800

    Lint files

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,18 +1,10 @@
 const bcrypt = require('bcrypt');
 
-//Create a password salt
+// Create a password salt
 const salt = bcrypt.genSaltSync(10);
 
 module.exports = {
   hashPassword(passwordFromUser) {
     return bcrypt.hashSync(passwordFromUser, salt);
   },
-  checkForPassword(passwordFromUser, rows) {
-    //if password matches
-    if (bcrypt.hashSync(passwordFromUser, salt) === rows[0].password) {
-      return true;
-    }
-    
-    return false;
-  }
-}
+};
--- server/utils/routes.js
+++ server/utils/routes.js
@@ -3,4 +3,6 @@ const user = require('../controllers/userController');
 module.exports = (app) => {
   app.route('/users')
     .post(user.createUser);
+  app.route('/users/login')
+    .post(user.loginUser);
 };
