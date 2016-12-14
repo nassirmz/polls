9c883ad27cb60301d825df936f64db641bf8b55d
nassir <nassirmz@gmail.com>
2016-12-13 20:22:11 -0800

    Add loginuser function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,33 @@
 const db = require('../db');
 const helpers = require('../utils/helpers.js');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   createUser(req, res, next) {
-    let queryStr = 'Insert into users (name, email, password) values ($1, $2, $3) returning id;';
-    console.log('what is req body', req.body);
-    let { name, email, password } = req.body;
-    let hashed_pwd = helpers.hashPassword(password);
-    console.log('from database', name, email, hashed_pwd);
-    db.query(queryStr, [name, email, hashed_pwd])
-      .then(function (data) {
+    const queryStr = 'Insert into users (name, email, password) values ($1, $2, $3) returning id;';
+    const { name, email, password } = req.body;
+    const hashedPwd = helpers.hashPassword(password);
+    db.query(queryStr, [name, email, hashedPwd])
+      .then((data) => {
         res.json(data);
       })
-      .catch(function (err) {
+      .catch((err) => {
         next(err);
+      });
+  },
+  loginUser(req, res, next) {
+    const queryStr = 'Select * from users where name = $1 and password = $2';
+    const { name, password } = req.body;
+    const hashedPwd = helpers.hashPassword(password);
+    db.query(queryStr, [name, hashedPwd])
+      .then((data) => {
+        // create a token and send it
+        const token = jwt.sign({ name }, 'mynameistom');
+        console.log(token, data,  'TOKEN');
+        res.json({ ...data, token });
       })
-  }
-}
+      .catch((err) => {
+        next(err);
+      });
+  },
+};
