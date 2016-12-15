e50773277e9e84f14a72540795b206e1b4c017b2
nassir <nassirmz@gmail.com>
2016-12-15 00:00:25 -0800

    Add delete to routes

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -2,7 +2,8 @@ const user = require('../controllers/userController');
 
 module.exports = (app) => {
   app.route('/users')
-    .post(user.createUser);
+    .post(user.createUser)
+    .delete(user.deleteUser);
   app.route('/users/login')
     .post(user.loginUser);
 };
