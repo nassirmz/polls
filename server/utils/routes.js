e159f7e85df7cc9647c8090bc56ae161e6a949d1
nassir <nassirmz@gmail.com>
2016-12-17 01:27:44 -0800

    Add router based middleware to routes

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -3,9 +3,9 @@ const user = require('../controllers/userController');
 module.exports = (app) => {
   app.route('/users')
     .post(user.createUser)
-    .delete(user.deleteUser);
+    .delete(user.checkAuth, user.deleteUser);
   app.route('/users/login')
     .post(user.loginUser);
   app.route('/users/logout')
-    .get(user.logoutUser);
+    .get(user.checkAuth, user.logoutUser);
 };
