4af65b6c6217f3dbbb1fbc3da1efd45b269c8868
nassir <nassirmz@gmail.com>
2016-12-16 17:40:23 -0800

    Add error handling middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const app = express();
+const helpers = require('./utils/helpers');
 
 require('./utils/middleware')(app, express);
 require('./utils/routes')(app);
+
+// use error handling middleware
+app.use(helpers.errorHandler);
 
 module.exports = app;
--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,10 +1,5 @@
-const bcrypt = require('bcrypt');
-
-// Create a password salt
-const salt = bcrypt.genSaltSync(10);
-
 module.exports = {
-  hashPassword(passwordFromUser) {
-    return bcrypt.hashSync(passwordFromUser, salt);
+  errorHandler(err, req, res, next, statusCode) {
+    res.status(statusCode).json(err);
   },
 };
