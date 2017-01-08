779e6f394a87aab56dd2fcf040eea1835332fcca
nassir <nassirmz@gmail.com>
2017-01-07 21:02:43 -0800

    Add Resolve object to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
 module.exports = {
   entry: './src/index.jsx',
+
   output: {
     path: __dirname,
     filename: '/public/bundle.js',
   },
+
+  resolve: {
+    root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './src/components',
+      './src/containers',
+    ],
+    extensions: ['', '.js', '.jsx'],
+  },
+
   module: {
     loaders: [
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
+          presets: ['react', 'es2015', 'stage-0'],
         },
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
       },
       {
         loader: 'style!css',
-        test: /\.css$?/,
+        test: /\.css?$/,
       },
     ],
   },
