1022dce9ffa1047b35b3f160c120a43a8a19187f
nassir <nassirmz@gmail.com>
2017-01-08 23:23:24 -0800

    Add a container div

--- src/containers/Signup.jsx
+++ src/containers/Signup.jsx
@@ -4,42 +4,44 @@ import { Form, FormGroup, Col, FormControl, Button, ControlLabel } from 'react-b
 class Signup extends Component {
   render() {
     return (
-      <Form horizontal>
-        <FormGroup controlId="formHorizontalEmail">
-          <Col componentClass={ControlLabel} sm={2}>
-            Username
-          </Col>
-          <Col sm={10}>
-            <FormControl type="text" placeholder="Email" />
-          </Col>
-        </FormGroup>
+      <Col className="auth-container" sm={6} smOffset={3}>
+        <Form horizontal>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Username
+            </Col>
+            <Col sm={10}>
+              <FormControl type="text" />
+            </Col>
+          </FormGroup>
 
-        <FormGroup controlId="formHorizontalEmail">
-          <Col componentClass={ControlLabel} sm={2}>
-            Email
-          </Col>
-          <Col sm={10}>
-            <FormControl type="email" placeholder="Email" />
-          </Col>
-        </FormGroup>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+            </Col>
+            <Col sm={10}>
+              <FormControl type="email" />
+            </Col>
+          </FormGroup>
 
-        <FormGroup controlId="formHorizontalPassword">
-          <Col componentClass={ControlLabel} sm={2}>
-            Password
-          </Col>
-          <Col sm={10}>
-            <FormControl type="password" placeholder="Password" />
-          </Col>
-        </FormGroup>
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+            </Col>
+            <Col sm={10}>
+              <FormControl type="password" />
+            </Col>
+          </FormGroup>
 
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Button type="submit">
-              Sign in
-            </Button>
-          </Col>
-        </FormGroup>
-      </Form>
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">
+                Sign in
+              </Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </Col>
     );
   }
 }
