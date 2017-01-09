719cae8a210ef5cb7ff2dbb1c85cc65903e761c9
nassir <nassirmz@gmail.com>
2017-01-08 23:11:26 -0800

    Scaffold Routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from 'App';
+import Signup from 'Signup';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="signup" component={Signup} />
+    </Route>
+  </Router>,
   document.getElementById('app'),
 );
