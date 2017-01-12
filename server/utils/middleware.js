7e6f1cce3cdb7bd3e49d161bd4e62dbf2dc33bf2
nassir <nassirmz@gmail.com>
2017-01-11 20:59:22 -0800

    Change back endpoints

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -8,5 +8,5 @@ module.exports = (app, express) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static(path.join(__dirname, '/../../public')));
-  app.use(expressJWT({ secret: 'mynameistom' }).unless({ path: ['/signup', '/signin', '/'] }));
+  app.use(expressJWT({ secret: 'mynameistom' }).unless({ path: ['/users', '/users/login', '/'] }));
 };
