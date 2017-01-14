8c7913c203e6a55e0c6785a4e466f8a239c94acd
nassir <nassirmz@gmail.com>
2017-01-13 23:49:01 -0800

    Define auth action creators

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,2 +1,38 @@
+import axios from 'axios';
+import { hashHistory } from 'react-router';
+
 import { AUTH_SUCCESS, AUTH_FAILURE } from '../constants/ActionTypes';
+
+export function authSuccess(name) {
+  return {
+    type: AUTH_SUCCESS,
+    name,
+    authenticated: true,
+  };
+}
+
+export function authFailure(errorMessage) {
+  return {
+    type: AUTH_FAILURE,
+    errorMessage,
+  };
+}
+
+export function signup(name, email, password) {
+  return (dispatch) => {
+    axios.post('/users', {
+      name,
+      email,
+      password,
+    })
+    .then((resp) => {
+      window.localStorage.setItem('Auth', resp.headers.auth);
+      dispatch(authSuccess(resp.data.username));
+      hashHistory.push('/polls');
+    })
+    .catch(() => {
+      dispatch.authFailure('Email is unavailable');
+    });
+  };
+}
 
